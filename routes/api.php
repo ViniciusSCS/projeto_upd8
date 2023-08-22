691d46b12386f4c9cf4bb128892d0f1cb263b4bc
Vinícius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2023-08-22 16:42:50 -0300

    Adicionando login

--- routes/api.php
+++ routes/api.php
@@ -44,21 +44,3 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/select/{codigo_uf}', [CidadeController::class, 'selectPorEstado']);
     });
 });
-
-// Route::prefix('cliente')->group(function () {
-//     Route::post('/cadastro', [ClienteController::class, 'create']);
-//     Route::get('/listar', [ClienteController::class, 'list']);
-//     Route::get('/editar/{id}', [ClienteController::class, 'edit']);
-//     Route::get('/visualizar/{id}', [ClienteController::class, 'show']);
-//     Route::get('/buscar', [ClienteController::class, 'search']);
-//     Route::put('/atualizar/{id}', [ClienteController::class, 'update']);
-//     Route::delete('/deletar/{id}', [ClienteController::class, 'destroy']);
-// });
-
-// Route::prefix('estado')->group(function () {
-//     Route::get('/select', [EstadoController::class, 'select']);
-// });
-
-// Route::prefix('cidade')->group(function () {
-//     Route::get('/select/{codigo_uf}', [CidadeController::class, 'selectPorEstado']);
-// });
