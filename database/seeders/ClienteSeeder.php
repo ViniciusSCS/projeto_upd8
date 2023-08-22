854718d642aa92c787c3ffb5ea65d53da2f4ff74
Vinícius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2023-08-22 12:31:07 -0300

    Atualizando seeders

--- database/seeders/ClienteSeeder.php
+++ database/seeders/ClienteSeeder.php
@@ -37,6 +37,90 @@ class ClienteSeeder extends Seeder
                 "data_nascimento" => "1989-04-20",
                 "sexo" => "Masculino"
             ],
+            [
+                "nome" => "Giovana Clara da Conceição",
+                "cpf" => "576.643.241-06",
+                'endereco_id' => 4,
+                "data_nascimento" => "1956-01-25",
+                "sexo" => "Feminino"
+            ],
+            [
+                "nome" => "Vanessa Louise Barros",
+                "cpf" => "442.398.281-12",
+                'endereco_id' => 5,
+                "data_nascimento" => "2003-01-18",
+                "sexo" => "Feminino"
+            ],
+            [
+                "nome" => "Rafael Gael Fábio Assunção",
+                "cpf" => "006.638.046-47",
+                'endereco_id' => 6,
+                "data_nascimento" => "1951-02-08",
+                "sexo" => "Masculino"
+            ],
+            [
+                "nome" => "Marli Andrea Nicole da Luz",
+                "cpf" => "843.030.952-73",
+                'endereco_id' => 7,
+                "data_nascimento" => "1952-06-22",
+                "sexo" => "Feminino"
+            ],
+            [
+                "nome" => "Rosângela Tânia Drumond",
+                "cpf" => "037.036.106-70",
+                'endereco_id' => 8,
+                "data_nascimento" => "1951-02-05",
+                "sexo" => "Feminino"
+            ],
+            [
+                "nome" => "Bernardo Alexandre Moura",
+                "cpf" => "781.046.353-52",
+                'endereco_id' => 9,
+                "data_nascimento" => "1992-03-18",
+                "sexo" => "Masculino"
+            ],
+            [
+                "nome" => "Natália Julia Carolina Lima",
+                "cpf" => "085.063.666-31",
+                'endereco_id' => 10,
+                "data_nascimento" => "1951-03-12",
+                "sexo" => "Feminino"
+            ],
+            [
+                "nome" => "Laura Rafaela Patrícia Souza",
+                "cpf" => "531.402.801-81",
+                'endereco_id' => 11,
+                "data_nascimento" => "1995-02-23",
+                "sexo" => "Feminino"
+            ],
+            [
+                "nome" => "Pietro Nathan Nicolas Lopes",
+                "cpf" => "519.274.378-81",
+                'endereco_id' => 12,
+                "data_nascimento" => "1993-01-08",
+                "sexo" => "Masculino"
+            ],
+            [
+                "nome" => "Thiago Campos",
+                "cpf" => "390.567.472-66",
+                'endereco_id' => 13,
+                "data_nascimento" => "1998-06-10",
+                "sexo" => "Masculino"
+            ],
+            [
+                "nome" => "Marcos Brito",
+                "cpf" => "199.236.598-95",
+                'endereco_id' => 14,
+                "data_nascimento" => "1988-07-12",
+                "sexo" => "Masculino"
+            ],
+            [
+                "nome" => "Débora Lorena Elza da Rocha",
+                "cpf" => "328.001.708-40",
+                'endereco_id' => 15,
+                "data_nascimento" => "1998-03-27",
+                "sexo" => "Feminino"
+            ],
         ];
 
         foreach ($clientes as $cliente) {
--- database/seeders/EnderecoSeeder.php
+++ database/seeders/EnderecoSeeder.php
@@ -27,6 +27,54 @@ class EnderecoSeeder extends Seeder
                 "endereco" => 'Rua YY',
                 "cidade_id" => 3200
             ],
+            [
+                "endereco" => 'Rua dos Reis Magos',
+                "cidade_id" => 3114
+            ],
+            [
+                "endereco" => 'Rua João Pereira de Carvalho',
+                "cidade_id" => 990
+            ],
+            [
+                "endereco" => 'Travessa Avestruz',
+                "cidade_id" => 68
+            ],
+            [
+                "endereco" => 'Quadra G',
+                "cidade_id" => 883
+            ],
+            [
+                "endereco" => 'Avenida Historiador Raimundo Girão',
+                "cidade_id" => 950
+            ],
+            [
+                "endereco" => 'Rua dos Jasmins',
+                "cidade_id" => 4754
+            ],
+            [
+                "endereco" => 'Avenida Camapuã',
+                "cidade_id" => 112
+            ],
+            [
+                "endereco" => 'Rua Caparari',
+                "cidade_id" => 112
+            ],
+            [
+                "endereco" => 'Rua Caterete',
+                "cidade_id" => 5123
+            ],
+            [
+                "endereco" => 'Comunidade Jucuruaba',
+                "cidade_id" => 3171
+            ],
+            [
+                "endereco" => 'Rua Claudomiro de Moraes, s/n',
+                "cidade_id" => 302
+            ],
+            [
+                "endereco" => 'Rua Natal',
+                "cidade_id" => 827
+            ],
         ];
 
         foreach ($enderecos as $endereco) {
